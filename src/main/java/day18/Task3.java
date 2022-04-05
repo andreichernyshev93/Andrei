51b73c1e90012b3fec073b1202a9ffc2444f3581
andrei.chernyshev93 <andrei.chernyshev93@mail.ru>
2022-04-05 14:47:51 +0300

    complete day 18 tasks

---
+++ src/main/java/day18/Task3.java
@@ -0,0 +1,78 @@
+package day18;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Node root = new Node(20);
+
+        int [] numbers = {14, 23, 11, 16, 22, 27, 5, 15, 18, 24, 150, 8};
+
+        for (int number : numbers)
+            addNode(number, root);
+
+        dfs(root);
+    }
+
+    private static void dfs(Node node) {
+        if (node == null)
+            return;
+
+        dfs(node.getLeft());
+        System.out.print(node.getValue() + " ");
+        dfs(node.getRight());
+    }
+
+    private static void addNode(int addValue, Node root) {
+        if (root == null)
+            throw new IllegalArgumentException("Значение корневого узла не может быть пустым");
+
+        Node nextNode = root;
+        Node current = null;
+
+        while (nextNode != null) {
+            current = nextNode;
+
+            if (addValue < current.getValue()) {
+                nextNode = current.getLeft();
+            } else {
+                nextNode = current.getRight();
+            }
+        }
+
+       if (addValue < current.getValue()) {
+           current.setLeft(new Node(addValue));
+       } else {
+           current.setRight(new Node(addValue));
+       }
+    }
+
+}
+
+class Node {
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+}
